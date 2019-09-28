4b5d0d35ca2372a27e3999cd01a0980fc4e44675
unknown <prak2481@prak2481.ksplad.com>
2019-09-28 13:56:33 +0530

    comment the line

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,6 +23,8 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  
+ }
 
-export const routingComponent = [AdditionDeletionComponent, MovementTrackerComponent]
+//export const routingComponent = [AdditionDeletionComponent, MovementTrackerComponent]
