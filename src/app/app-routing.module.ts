f3437f70d888f20753cfc949f93e40f6e0f37ff6
prakash <prakashjai0691@outlook.com>
2019-09-29 21:30:58 +0530

    29092019

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './Employee_Masters/login/login.component';
-import { AdditionDeletionComponent } from './Employee_Masters/master-data/addition-deletion/addition-deletion.component';
-import { MovementTrackerComponent } from './Employee_Masters/master-data/movement-tracker/movement-tracker.component';
+import { AdditionDeletionComponent } from './Employee_Masters/addition-deletion/addition-deletion.component';
+import { MovementTrackerComponent } from './Employee_Masters/movement-tracker/movement-tracker.component';
+import { ResourcesMasterComponent } from './Employee_Masters/resources-master/resources-master.component';
 
 
 
 
 const routes: Routes = [
-{path: 'Addition-Deletion', component: AdditionDeletionComponent},
-{path: 'Movement-Tracker', component: MovementTrackerComponent},
 
 
   ];
@@ -27,4 +26,4 @@ export class AppRoutingModule {
   
  }
 
-//export const routingComponent = [AdditionDeletionComponent, MovementTrackerComponent]
+export const routingComponent = [AdditionDeletionComponent, MovementTrackerComponent, ResourcesMasterComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule, routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomDirectives } from './custom.directives';
-import { MasterDataModule } from './Employee_Masters/master-data/master-data.module';
 import { LoginComponent } from './Employee_Masters/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatSortModule, MatTableModule, MatDatepickerModule, MatFormFieldModule } from '@angular/material';
-import { OtherModulesModule } from './Employee_Masters/other-modules/other-modules.module';
+import { RouterModule } from '@angular/router';
+import { AdditionDeletionComponent } from './Employee_Masters/addition-deletion/addition-deletion.component';
+import { MovementTrackerComponent } from './Employee_Masters/movement-tracker/movement-tracker.component';
+import { ResourcesMasterComponent } from './Employee_Masters/resources-master/resources-master.component';
+import { BreadcrumbComponent } from './Employee_Masters/breadcrumb/breadcrumb.component';
+import { DashboardComponent } from './Employee_Masters/dashboard/dashboard.component';
 
 
 
@@ -18,12 +22,15 @@ import { OtherModulesModule } from './Employee_Masters/other-modules/other-modul
     CustomDirectives,
     LoginComponent,
     routingComponent,
-    
-   
-
-
+    AdditionDeletionComponent,
+    MovementTrackerComponent,
+    ResourcesMasterComponent,
+    BreadcrumbComponent,
+    DashboardComponent
 
   ],
+
+
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -35,7 +42,19 @@ import { OtherModulesModule } from './Employee_Masters/other-modules/other-modul
     MatTableModule,
     MatDatepickerModule,
     MatFormFieldModule,
-    OtherModulesModule
+    RouterModule.forRoot([
+      { path: '', component: LoginComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'Addition-Deletion', component: AdditionDeletionComponent },
+      { path: 'Addition-Deletion', component: AdditionDeletionComponent },
+      { path: 'Movement-Tracker', component: MovementTrackerComponent },
+      { path: 'Resources-Master', component: ResourcesMasterComponent },
+      { path: 'Dashboard', component: DashboardComponent },
+
+
+
+    ])
+
 
 
 
